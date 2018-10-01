d8a0595f463ac3fc503032bedf46801815cb8671
Igor Plotnikov <ig.plotnikov@gmail.com>
2018-10-02 00:08:15 +0300

    Add Unicode suits

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,13 @@
+if Gem.win_platform?
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+
 values = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-suits = %w(Diamonds Hearts Clubs Spades)
+suits = ["♦", "♠", "♥ ", "♣"]
 
 puts "#{values.sample} of #{suits.sample}"
